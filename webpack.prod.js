d1eae815c3caa00997810fb02d20a73df7228477
bricechou <bricechou@gmail.com>
2020-06-11 16:07:04 +0800

    add source-map comment into webpack.prod.js file

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,8 @@ const common = require('./webpack.config.js')
 
 module.exports = merge(common, {
   mode: 'production',
+  // if you want to debug your code on production enviornment, add 'source-map' into output file.
+  // devtool: 'source-map',
   output: {
     filename: '[name].js',
   },
