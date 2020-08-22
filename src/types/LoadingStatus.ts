8300f4a0cc5cb1046109a7e02981eb9e360db54f
bricechou <bricechou@gmail.com>
2020-08-22 18:12:53 +0800

    add loading type enum

--- src/types/LoadingStatus.ts
+++ src/types/LoadingStatus.ts
@@ -1,7 +1,14 @@
 /** @format */
 
-export enum StatusEnum {
+export enum LoadingStatusEnum {
   Failed,
   Loading,
   Success,
+}
+
+export enum LoadingTypeEnum {
+  // 页面
+  Page,
+  // 浮层
+  Modal,
 }
