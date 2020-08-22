395ab70d81e028e7683b500f1dd6beecaf26b774
bricechou <bricechou@gmail.com>
2020-08-22 18:10:24 +0800

    update mock request failed prob. params.

--- src/mock/request.ts
+++ src/mock/request.ts
@@ -1,13 +1,15 @@
-/** @format */
-
 /**
  * 前端如何优雅地模拟接口请求？(给你的代码加点小意外) - BriceChou的文章 - 知乎
  * https://zhuanlan.zhihu.com/p/143549764
  *
+ * 随机数据生成配上 vscode-faker 更香哦！
+ * https://marketplace.visualstudio.com/items?itemName=deerawan.vscode-faker
+ *
  * @author bricechou@gmail.com
  */
+
 import type { DataStatusType } from '../types/api/dataStatus'
-import { random, md5SafeAdd, PROB_TENTH } from '../utils/common'
+import { random, md5SafeAdd, PROB_TENTH, isObjectNull } from '../utils/common'
 
 export interface DataErrorType {
   data: any
@@ -16,22 +18,31 @@ export interface DataErrorType {
 }
 
 function handleError(res: any, rj: any) {
-  const status: DataStatusType = (res && res.status) || {}
+  const status: DataStatusType = res?.status || {}
   rj({
     data: res,
-    errorCode: status.code || 500,
-    errorMsg: status.message || '垃圾服务器被网络挤爆啦～',
+    errorCode: status?.code || 500,
+    errorMsg: status?.message || '垃圾服务器被网络挤爆啦～',
   })
 }
 
 function handleData(res: any) {
-  if (res && res.status && res.status.code == 400) {
+  if (res?.status?.code == 400) {
     return res
   }
   throw res
 }
 
-export function request<T>(data: T): Promise<T> {
+/**
+ * 模拟接口请求
+ *
+ * @param data 模拟数据回传状态体
+ * @param params 用于检查请求参数
+ */
+export function request<T>(data: T, params = {}): Promise<T> {
+  if (!isObjectNull(params)) {
+    console.log(params)
+  }
   const timeout = random([0.1, 0.2, 0.5, 1, 2], [0.2, 0.3, 0.3, 0.1, 0.1]) * 1000
   return new Promise((rs, rj) => {
     setTimeout(() => {
@@ -47,9 +58,9 @@ export function request<T>(data: T): Promise<T> {
 }
 
 /**
- * 请求失败接口回传
+ * 请求接口状态体回传
  *
- * @param count 失败阀值系数
+ * @param count 失败概率阀值系数
  */
 export function getDataStatusMockData(threshold = 1): DataStatusType {
   let res: DataStatusType = {
@@ -59,9 +70,11 @@ export function getDataStatusMockData(threshold = 1): DataStatusType {
       signature: 0,
     },
     message = ''
-  const failedProb = threshold * 0.05
   const timestamp = new Date().getTime()
-  const code = random([400, 200, void 0], [0.95 - failedProb, failedProb, 0.05])
+  const failedProb = threshold * 0.05
+  const successProb = failedProb >= 0.95 ? 0.05 : 0.95 - failedProb
+  // 400：失败 200：成功
+  const code = random([400, 200, void 0], [successProb, failedProb, 0.05])
   if (code != 400) {
     message = random(
       [
